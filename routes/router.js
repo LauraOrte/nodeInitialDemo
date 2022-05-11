5b82ebc7eaa9ce6eeb8449a1bae1acd47be238ea
LauraOrte <95409913+LauraOrte@users.noreply.github.com>
2022-05-11 09:14:30 +0200

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -23,15 +23,15 @@ const
   
 juego.post('/login',checkAdmin)
 juego.use( checkJWT)
-juego.post('/players', addNewPlayer) //POST  crea un jugador   --Works
-juego.post('/players/:id/games', playerRollDices) //POST /players/{id}/games: un jugador específic realitza una tirada --Works
-juego.put('/players/:id', modifyPlayerName) //PUT  modifica el nom del jugador --Works
-juego.delete('/players/:id/games', deleteGames) // DELETE /players/{id}/games: elimina les tirades del jugador --Works
-juego.get('/players', getAllPlayers)//GET : retorna el llistat de tots els jugadors del sistema amb el seu percentatge mig d’èxits --Works
-juego.get('/players/:id/games', playerGamesList)//GET /players/{id}/games: retorna el llistat de jugades per un jugador. --Works
-juego.get('/players/ranking', generalRanking) //GET : retorna el percentatge mig d’èxits del conjunt de tots els jugadors --Works
-juego.get('/players/ranking/loser', getWorstPlayer )//GET : retorna el jugador amb pitjor percentatge d’èxit --Works
-juego.get('/players/ranking/winner', getBetterPlayer)//GET : retorna el jugador amb millor percentatge d’èxit  --Works
+juego.post('/players', addNewPlayer) 
+juego.post('/players/:id/games', playerRollDices) 
+juego.put('/players/:id', modifyPlayerName) 
+juego.delete('/players/:id/games', deleteGames) 
+juego.get('/players', getAllPlayers)
+juego.get('/players/:id/games', playerGamesList)
+juego.get('/players/ranking', generalRanking) 
+juego.get('/players/ranking/loser', getWorstPlayer )
+juego.get('/players/ranking/winner', getBetterPlayer)
 
 
 module.exports = juego
