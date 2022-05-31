a4844b0556d10137f347c63d89dec829d2d2b642
LauraOrte <laura.orte7@gmail.com>
2022-05-31 10:32:06 +0200

    ok

--- routes/router.js
+++ routes/router.js
@@ -20,15 +20,17 @@ const
     checkJWT
   } = require('../services/jwtManagment/jwt')
 
-  
+//rutas jugadores
 juego.post('/login',checkAdmin)
 juego.use( checkJWT)
 juego.post('/players', addNewPlayer) 
-juego.post('/players/:id/games', playerRollDices) 
 juego.put('/players/:id', modifyPlayerName) 
+//rutas juego
+juego.post('/players/:id/games', playerRollDices) 
 juego.delete('/players/:id/games', deleteGames) 
 juego.get('/players', getAllPlayers)
 juego.get('/players/:id/games', playerGamesList)
+//rutas ranking
 juego.get('/players/ranking', generalRanking) 
 juego.get('/players/ranking/loser', getWorstPlayer )
 juego.get('/players/ranking/winner', getBetterPlayer)
